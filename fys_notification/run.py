8b890c3ec9d2b9986d3dd945474144f43dda8514
seenureddy <seenu@VenkatMbp-1.local>
2021-03-20 22:06:07 +0530

    Commented the db context

--- fys_notification/run.py
+++ fys_notification/run.py
@@ -50,8 +50,8 @@ def create_app():
     # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     return app
 
