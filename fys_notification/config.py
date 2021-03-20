ec024f985469d4262056f9481196bdf56acdec38
seenureddy <seenu@VenkatMbp-1.local>
2021-03-20 20:57:59 +0530

    Adding the procfile and change the heroku settings

--- fys_notification/config.py
+++ fys_notification/config.py
@@ -21,12 +21,9 @@ FYS_SMPT_PASSWORD = os.getenv("FYS_SMPT_PASSWORD")
 FYS_ADMIN_EMAIL = os.getenv("FYS_ADMIN_EMAIL", "srinivasulur55.s@gmail.com")
 
 
-if get_env("LIFE_CYCLE") == "heroku" or get_env("LIFE_CYCLE") == "prod":
+if get_env("LIFE_CYCLE") == "heroku":
     config_file_logger.info(get_env("LIFE_CYCLE"))
-    POSTGRES_USER = get_env("POSTGRES_USER")
-    POSTGRES_URL = get_env("POSTGRES_URL")
-    POSTGRES_DB = get_env("POSTGRES_DB")
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}@{POSTGRES_URL}/{POSTGRES_DB}"
+    SQLALCHEMY_DATABASE_URI = get_env("DATABASE_URL")
 else:
     config_file_logger.info("local")
     POSTGRES_USER = "seenu"
